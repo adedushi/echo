f6908d0ed975378232fe3742f77f018118dee5e9
Albert Dedushi <adedushi@hotmail.com>
2024-01-28 16:06:31 -0500

    Implement JWT authentication

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,8 +1,12 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { secretOrKey } = require('./keys');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
+const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
+
 
 passport.use(new LocalStrategy({
     session: false,
@@ -18,3 +22,47 @@ passport.use(new LocalStrategy({
     } else
         done(null, false);
 }));
+
+exports.loginUser = async function (user) {
+    const userInfo = {
+        _id: user._id,
+        username: user.username,
+        email: user.email
+    };
+    const token = await jwt.sign(
+        userInfo, 
+        secretOrKey,
+        { expiresIn: 3600 } 
+    );
+    return {
+        user: userInfo,
+        token
+    };
+};
+
+const options = {};
+options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+options.secretOrKey = secretOrKey;
+
+passport.use(new JwtStrategy(options, async (jwtPayload, done) => {
+    try {
+        const user = await User.findById(jwtPayload._id)
+        if (user) {
+            return done(null, user);
+        }
+        return done(null, false);
+    }
+    catch (err) {
+        done(err);
+    }
+}));
+
+exports.requireUser = passport.authenticate('jwt', { session: false });
+
+exports.restoreUser = (req, res, next) => {
+    return passport.authenticate('jwt', { session: false }, function (err, user) {
+        if (err) return next(err);
+        if (user) req.user = user;
+        next();
+    })(req, res, next);
+};
