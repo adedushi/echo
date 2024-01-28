daf10388f6c570bed2a34ddc1c8a517f7a32ed0f
Albert Dedushi <adedushi@hotmail.com>
2024-01-28 15:39:27 -0500

    Update express error-handling

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const debug = require('debug');
 
 const cors = require('cors');
 const csurf = require('csurf');
@@ -35,6 +36,25 @@ app.use('/api/users', usersRouter);
 app.use('/api/echos', echosRouter);
 app.use('/api/csrf', csrfRouter);
 
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.statusCode = 404;
+    next(err);
+});
+
+const serverErrorLogger = debug('backend:error');
+
+app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    res.json({
+        message: err.message,
+        statusCode,
+        errors: err.errors
+    })
+});
 
 module.exports = app;
 
