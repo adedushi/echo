50876cfbb50188fa70f998d6a69c0a4598141c55
lsherman98 <122409681+lsherman98@users.noreply.github.com>
2024-02-07 13:39:37 -0500

    limit record length

--- frontend/src/components/Echos/EchoRecorder/EchoRecorder.jsx
+++ frontend/src/components/Echos/EchoRecorder/EchoRecorder.jsx
@@ -11,6 +11,7 @@ const EchoRecorder = ({ setAudio, setAudioUrl }) => {
         isRecording,
         recordingTime,
     } = useAudioRecorder();
+    
     const addAudioElement = useCallback((blob) => {
         const url = URL.createObjectURL(blob);
         const audio = new Audio()
@@ -24,11 +25,11 @@ const EchoRecorder = ({ setAudio, setAudioUrl }) => {
         addAudioElement(recordingBlob)
     }, [addAudioElement, recordingBlob])
 
-    // useEffect(() => {
-    //     if (recordingTime >= 30) {
-    //         stopRecording()
-    //     }
-    // }, [recordingTime])
+    useEffect(() => {
+        if (recordingTime >= 30) {
+            stopRecording()
+        }
+    }, [recordingTime, stopRecording])
 
     const handleClick = () => {
         if (isRecording) {
@@ -38,70 +39,15 @@ const EchoRecorder = ({ setAudio, setAudioUrl }) => {
         }
     }
 
-
     return (
         <>
-
             <div className={`${isRecording ? 'audio-recorder-is-recording' : 'audio-recorder-not-recording' }` } onClick={handleClick}>
                 {!isRecording && <i className={`fa-solid fa-microphone audio-recorder-mic`}></i>}
                 {isRecording && <p className={`audio-recorder-count`}>{recordingTime < 10 ? `0${recordingTime}` : `${recordingTime}`}</p>}
                 {isRecording && <RecordingAnimation />}
             </div>
-            {/* // startRecording	Invoking this method starts the recording. Sets isRecording to true
-                // stopRecording	Invoking this method stops the recording in progress and the resulting audio is made available in recordingBlob. Sets isRecording to false
-                // togglePauseResume	Invoking this method would pause the recording if it is currently running or resume if it is paused. Toggles the value isPaused
-                // recordingBlob	This is the recording blob that is created after stopRecording has been called
-                // isRecording	A boolean value that represents whether a recording is currently in progress
-                // isPaused	A boolean value that represents whether a recording in progress is paused
-                // recordingTime	Number of seconds that the recording has gone on. This is updated every second
-                // mediaRecorder	The current mediaRecorder in use. Can be undefined in case recording is not in progress */}
-            {/* <AudioRecorder 
-                onRecordingComplete={addAudioElement}
-                showVisualizer={true}
-                audioTrackConstraints={{
-                    noiseSuppression: true,
-                    echoCancellation: true,
-                }} 
-                downloadOnSavePress={false}
-                downloadFileExtension="mp3"
-                recorderControls={recorderControls}
-            /> */}
         </>
     )
 }
-
-
-
 export default EchoRecorder
 
-
-
-// startRecording	Invoking this method starts the recording. Sets isRecording to true
-// stopRecording	Invoking this method stops the recording in progress and the resulting audio is made available in recordingBlob. Sets isRecording to false
-// togglePauseResume	Invoking this method would pause the recording if it is currently running or resume if it is paused. Toggles the value isPaused
-// recordingBlob	This is the recording blob that is created after stopRecording has been called
-// isRecording	A boolean value that represents whether a recording is currently in progress
-// isPaused	A boolean value that represents whether a recording in progress is paused
-// recordingTime	Number of seconds that the recording has gone on. This is updated every second
-// mediaRecorder	The current mediaRecorder in use. Can be undefined in case recording is not in progress
-
-
-            /* <AudioRecorder 
-                onRecordingComplete={addAudioElement}
-                showVisualizer={true}
-                audioTrackConstraints={{
-                    noiseSuppression: true,
-                    echoCancellation: true,
-                }} 
-                downloadOnSavePress={false}
-                downloadFileExtension="mp3"
-                recorderControls={recorderControls}
-                classes={{
-                    AudioRecorderClass: 'audio-recorder-custom',
-                    AudioRecorderTimerClass: 'audio-recorder-timer',
-                    AudioRecorderStatusClass: 'audio-recorder-status',
-                    AudioRecorderStartSaveClass: 'audio-recorder-start-save',
-                    AudioRecorderPauseResumeClass: 'audio-recorder-pause-resume',
-                    AudioRecorderDiscardClass: 'audio-recorder-pause-discard'
-                }}
-            /> */
